9332e14a8a3e959b7bbf14d9e26b0d157a2c810a
joonyverse <joonyverse@gmail.com>
2025-07-10 01:03:54 +0900

    Update OAuth URL pattern for Netlify environment

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -19,8 +19,8 @@ export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
 const getURL = () => {
   let url =
     import.meta.env.VITE_SITE_URL ?? // Set this to your site URL in production env.
-    import.meta.env.VITE_VERCEL_URL ?? // Automatically set by Vercel.
-    'http://localhost:5173/'
+    import.meta.env.VITE_NETLIFY_URL ?? // Automatically set by Netlify.
+    (typeof window !== 'undefined' ? window.location.origin : 'http://localhost:5173')
   // Make sure to include `https://` when not localhost.
   url = url.startsWith('http') ? url : `https://${url}`
   // Make sure to include a trailing `/`.
